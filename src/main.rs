d9e55a24aa8356b6e77a6caafedc49126527a3d8
Jon Daniel <binarycleric@gmail.com>
2018-04-09 14:50:51 -0400

    adding basic set command

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,29 @@ use std::io::Write;
 use std::io::Read;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::thread;
+use std::str::FromStr;
+use std::collections::HashMap;
 
-fn process_request(stream: &mut TcpStream) {
+fn process_request(stream: &mut TcpStream, data_table: &mut HashMap<String, i32>) {
     let mut buffer = [0; 128];
 
     let payload_size = stream.read(&mut buffer).unwrap();
     let request_string = str::from_utf8(&buffer).unwrap();
 
     let mut arguments = request_string.split_whitespace();
+    let command = arguments.next().unwrap();
 
-    match arguments.next().unwrap() {
+    match command {
         "GET" => {
             println!("GET command");
         },
         "SET" => {
+            let identifier = arguments.next().unwrap();
+            let raw_value = arguments.next().unwrap();
+            let typed_value = i32::from_str(raw_value).unwrap();
+
+            data_table.insert(String::from(identifier), typed_value);
+
             println!("SET command");            
         }
         _ => {
@@ -28,19 +36,26 @@ fn process_request(stream: &mut TcpStream) {
     println!("accepted incoming connection.");
     println!("Size: {}", payload_size);
     println!("Request: {}", request_string);
+
+    println!("Data table: ");
+
+    for (key, value) in data_table {
+        println!("{} --- {}", key, value);        
+    }
+
     println!("\n\n");
 
     stream.write(b"Done.\r\n").unwrap();
 }
 
 fn main() {
+    let mut data_table = HashMap::new();
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+
     println!("listening started, ready to accept");
 
     for stream in listener.incoming() {
-        thread::spawn(|| {
-            let mut stream = stream.unwrap();
-            process_request(&mut stream);
-        });
+        let mut stream = stream.unwrap();
+        process_request(&mut stream, &mut data_table);
     }
 }
