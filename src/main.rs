c124746ba7dd7c1d9ff709e7927e3dacdc220068
Jon Daniel <binarycleric@gmail.com>
2018-04-12 17:51:24 -0400

    mass refactor

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,49 @@ pub mod redis {
         }
 
         #[derive(Debug, PartialEq)]
+        pub struct RedisType {
+            type_token: TypeTokens,
+        }
+
+        pub fn build_primitive(incoming: String) -> Primitives {
+            let rtype = RedisType::from_char(incoming.chars().next().unwrap());
+            let primitive = rtype.build_primitive(incoming);
+
+            return primitive;
+        }
+
+        impl RedisType {
+
+            // Need to double check that bulk strings actually work since thet
+            // can be up to 512mb in length and include "\r\n" characters.
+            pub fn build_primitive(&self, incoming: String) -> Primitives {
+                // Simple string. This code sucks.
+                if self.type_token == TypeTokens::SimpleString {
+                    let mut bundle = incoming.split("\r\n");
+                    let (_, simple_string) = bundle.next().unwrap().split_at(1);
+
+                    return Primitives::String(simple_string.to_string());
+                } else {
+                    let mut bundle = incoming.split("\r\n");
+                    let type_info = bundle.next().unwrap().to_string();
+                    let (rtype, size) = type_info.split_at(1);
+                    let data: Vec<String> = bundle.map(|s| s.to_string()).collect();
+
+                    return Primitives::String(data.join("\r\n").to_string());
+                }
+            }
+
+            // TODO: Actual error handling.
+            pub fn from_char(token: char) -> RedisType {
+                let type_token = TypeTokens::from_string(token).unwrap();
+
+                return RedisType {
+                    type_token: type_token,
+                }
+            }
+        }
+
+        #[derive(Debug, PartialEq)]
         enum TypeTokens {
             SimpleString,
             BulkString,
@@ -57,62 +100,16 @@ pub mod redis {
         }
 
         impl TypeTokens {
-            fn to_type_token(token: String) -> Option<TypeTokens> {
-                match token.as_str() {
-                    "+" => Some(TypeTokens::SimpleString),
-                    "$" => Some(TypeTokens::BulkString),
-                    ":" => Some(TypeTokens::Integer),
-                    "*" => Some(TypeTokens::Array),
+            fn from_string(token: char) -> Option<TypeTokens> {
+                match token {
+                    '+' => Some(TypeTokens::SimpleString),
+                    '$' => Some(TypeTokens::BulkString),
+                    ':' => Some(TypeTokens::Integer),
+                    '*' => Some(TypeTokens::Array),
                     _ => None,
                 }
             }
         }
-
-        #[derive(Debug, PartialEq)]
-        struct Incoming {
-            token: TypeTokens,
-            size: usize,
-            data: String,
-        }
-
-        impl Incoming {
-
-            // Need to double check that bulk strings actually work since thet
-            // can be up to 512mb in length and include "\r\n" characters.
-            fn to_primitive(&self) -> Option<Primitives> {
-                match self.token {
-                    TypeTokens::BulkString => {
-                        let (string, _) = self.data.split_at(self.size);
-                        return Some(Primitives::String(string.to_string()));
-                    },
-                    TypeTokens::SimpleString => {
-                        return Some(Primitives::String(self.data.to_string()));
-                    },
-                    _ => None
-                }
-            }
-
-            // TODO: Figure out size of the parsed incoming data.
-            // Maybe we don't even need to expose this since they only
-            // things that care about size are arrays and bulk strings.
-            fn size(&self) -> usize {
-                return 999;
-            }
-
-            fn from_string(incoming: String) -> Incoming {
-                let mut bundle = incoming.split("\r\n");
-                let type_info = bundle.next().unwrap().to_string();
-                let (rtype, size) = type_info.split_at(1);
-                let data: Vec<String> = bundle.map(|s| s.to_string()).collect();
-
-                return Incoming {
-                    token: TypeTokens::to_type_token(rtype.to_string()).unwrap(),
-                    size: size.parse::<usize>().unwrap(),
-                    data: data.join("\r\n"),
-                }
-            }
-        }
-
 
         #[cfg(test)]
         mod tests {
@@ -120,25 +117,19 @@ pub mod redis {
             #[test]
             fn it_builds_incoming_from_simple_string() {
                 let request = "+OK\r\n".to_string();
-                let incoming = super::Incoming::from_string(request);
+                let primitive = super::build_primitive(request);
                 let expected_primitive = super::Primitives::String("OK".to_string());
 
-                assert_eq!(incoming.token, super::TypeTokens::SimpleString);
-                assert_eq!(incoming.size, 2);
-                assert_eq!(incoming.data, "OK".to_string());
-                assert_eq!(incoming.to_primitive().unwrap(), expected_primitive);
+                assert_eq!(expected_primitive, primitive);
             }
 
             #[test]
             fn it_builds_incoming_from_bulk_string() {
                 let request = "$6\r\nselect".to_string();
-                let incoming = super::Incoming::from_string(request);
+                let primitive = super::build_primitive(request);
                 let expected_primitive = super::Primitives::String("select".to_string());
 
-                assert_eq!(incoming.token, super::TypeTokens::BulkString);
-                assert_eq!(incoming.size, 6);
-                assert_eq!(incoming.data, "select".to_string());
-                assert_eq!(incoming.to_primitive().unwrap(), expected_primitive);
+                assert_eq!(expected_primitive, primitive);
             }
         }
     }
