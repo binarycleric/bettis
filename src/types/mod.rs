1d103ea45ee088195c629a9e3f2dde7bb02d0c85
Jon Daniel <binarycleric@gmail.com>
2018-04-23 01:05:46 -0400

    Starting to use different types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,5 +1,12 @@
-mod qarray;
 mod data_type;
+mod qsimple_string;
+mod qarray;
 
-pub use self::qarray::QArray;
 pub use self::data_type::DataType;
+pub use self::qarray::QArray;
+pub use self::qsimple_string::QSimpleString;
+
+pub trait QDataType {
+    fn to_data_type(&self) -> DataType;
+    fn to_protocol(&self) -> String;
+}
--- src/types/qarray.rs
+++ src/types/qarray.rs
@@ -1,36 +1,55 @@
 use types::data_type::DataType;
+use types::QDataType;
+use types::QSimpleString;
 
-#[derive(Debug, PartialEq, Clone)]
-pub struct QArray {
-    values: Vec<DataType>,
+#[derive(Debug, Clone)]
+pub struct QArray<T> where T: QDataType + Clone {
+    values: Vec<T>,
 }
 
-impl QArray {
-    pub fn new() -> QArray {
+impl<T: QDataType + Clone> QArray<T> {
+    pub fn new() -> QArray<T> {
         QArray { values: Vec::new() }
     }
 
-    pub fn from_vec(values: Vec<DataType>) -> QArray {
+    pub fn from_vec(values: Vec<T>) -> QArray<T> {
         QArray { values: values }
     }
 }
 
-trait QDataType {
-    fn to_redis_protocol<'p>(&self) -> String;
-    fn to_data_type(&self) -> DataType;
-}
-
-impl QDataType for QArray {
+impl<T: QDataType + Clone> QDataType for QArray<T> {
     fn to_data_type(&self) -> DataType {
-        DataType::Array(self.values.to_vec())
+        let vec_values = Vec::new();
+
+        DataType::Array(vec_values)
     }
 
-    fn to_redis_protocol(&self) -> String {
+    fn to_protocol(&self) -> String {
         let mut retval = format!("*{}\r\n", self.values.len());
 
         for row in &self.values {
-            retval.push_str(&row.to_redis_protocol());
+            retval.push_str(&row.to_protocol());
         }
         return retval;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::QDataType;
+    use super::QArray;
+    use super::QSimpleString;
+
+    #[test]
+    fn it_exports_into_protocol() {
+        let bulk_strings = vec![
+            QSimpleString::from_string("Example".to_string()),
+            QSimpleString::from_string("Example".to_string()),
+        ];
+
+        let qarray: QArray<QSimpleString> = QArray::from_vec(bulk_strings);
+        let protocol = qarray.to_protocol();
+
+        assert_eq!("*2\r\n+Example\r\n+Example\r\n", protocol);
     }
 }
