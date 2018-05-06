0b8dbd0a03a01533997115a4089d03aafde60ee0
Jon Daniel <binarycleric@gmail.com>
2018-05-06 16:18:53 -0400

    Slimming down storage object.
    I don't want it to turn into a god object.

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -12,11 +12,6 @@ use self::resp::Value as DataValue;
 use std::collections::HashMap as DataHash;
 use std::collections::BTreeMap;
 
-const INVALID_INCR_ERROR: &'static str = "ERR value is not an integer or out of range";
-
-
-
-
 
 #[derive(Debug)]
 pub struct KeyStore {
@@ -117,50 +112,6 @@ impl Database {
             Some(duration) => Some(duration.remaining()),
             None => None,
         }
-    }
-
-    pub fn incr(&mut self, key: String) -> Result<DataValue, DataValue> {
-        let new_value: i64;
-
-        match self.get(key.clone()) {
-            Some(value) => {
-                if let &DataValue::Integer(ref int) = value {
-                    new_value = int.clone() + 1;
-                } else {
-                    return Err(DataValue::Error(INVALID_INCR_ERROR.to_string()));
-                }
-            }
-            None => {
-                new_value = 1;
-            }
-        }
-
-        self.set(key.clone(), DataValue::Integer(new_value));
-        Ok(DataValue::Integer(new_value))
-    }
-
-    pub fn decr(&mut self, key: String) -> Result<DataValue, DataValue> {
-        let new_value: i64;
-
-        match self.get(key.clone()) {
-            Some(value) => {
-                if let &DataValue::Integer(ref int) = value {
-                    new_value = int.clone() - 1;
-                } else {
-                    return Err(DataValue::Error(INVALID_INCR_ERROR.to_string()));
-                }
-            }
-            None => {
-                new_value = -1;
-            }
-        }
-
-        self.set(key.clone(), DataValue::Integer(new_value));
-        Ok(DataValue::Integer(new_value))
-    }
-
-    fn data_key(key: String) -> DataKey {
-        DataKey::new(key)
     }
 }
 
